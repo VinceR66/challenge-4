f7e8f3b93a2c11d9af77bfa03b624ad57f4bb130
VinceR66 <vrook@me.com>
2023-04-10 16:49:02 -0500

    timer added

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,22 @@
+var timerEl = document.querySelector("#timer");
 
+
+var secondsLeft = 10;
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timerEl.textContent = secondsLeft + " second(s) left";
+
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+        }
+    }, 1000);
+}
+
+function sendMessage() {
+    timerEl.textContent = "Game Over";
+}
+
+setTime();
