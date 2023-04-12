d0d32f6f1abdd3c86dd9e6847170b20fee4a7df4
VinceR66 <vrook@me.com>
2023-04-12 00:46:30 -0500

    questions

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
-var timerEl = document.querySelector("#timer");
 
+var timerEl = document.querySelector("#timer");
 
 var secondsLeft = 10;
 
@@ -20,3 +20,37 @@ function sendMessage() {
 }
 
 setTime();
+
+var questionsList = [
+    {
+        questionText: "how do I declare a variable in JavaScript",
+        choices: ["var", "for", "while", "if"],
+        correctAnswer: "var",
+    },
+    {
+        questionText: "how do I declare a variable in JavaScript",
+        choices: ["var", "for", "while", "if"],
+        correctAnswer: "var",
+    },
+    {
+        questionText: "how do I declare a variable in JavaScript",
+        choices: ["var", "for", "while", "if"],
+        correctAnswer: "var",
+    },
+];
+
+var questionsTextElement = document.getElementById("question-text");
+var choicesContainerElement = document.getElementById("choices-container");
+
+function main() {
+    questionsTextElement.textContent = questionsList[0].questionText;
+
+    for (var i = 0; i < questionsList[0].choices.length; i++) {
+        var newChoiceButton = document.createElement("button");
+        newChoiceButton.textContent = questionsList[0].choices[i];
+        choicesContainerElement.append(newChoiceButton);
+    }
+    console.log("This is my example");
+}
+
+main();
